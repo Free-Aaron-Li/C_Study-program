75fc0ad90878a61639a9c5b59fd3377cd5dc219a
AaronLi <Free_Aaron.Li@outlook.com>
2022-11-29 20:37:00 +0800

    字符频率直方图

---
+++ ChapterOneIntroduction/exercise/1-14-FrequencyOfWords.c
@@ -0,0 +1,34 @@
+/**
+ * @file 1-14-FrequencyOfWords.c
+ * @author AaronLi
+ * @date 2022-11-29-20
+ * @description 编写一个程序，打印输入中各个字符出现频率的直方图
+ * @link
+ */
+
+#include <stdio.h>
+
+int main() {
+    int c;
+    int words[26];      // Type of word
+    for (int i = 0; i < 26; ++i) {
+        words[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        if ((c>='a')&&(c<='z')) {
+            words[c-'a']++;
+        }
+    }
+
+    for (int m = 0; m < 26; ++m) {
+        int i;
+        putchar(m+'a');
+        putchar('|');
+        for (i = 0; i < words[i]; ++i) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
