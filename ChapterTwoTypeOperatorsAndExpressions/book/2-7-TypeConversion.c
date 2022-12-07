eb0fba574579770f4932b2b918f727b3397ee019
AaronLi <free_aaron.li@outlook.com>
2022-12-07 16:47:07 +0800

    更新

---
+++ ChapterTwoTypeOperatorsAndExpressions/book/2-7-TypeConversion.c
@@ -0,0 +1,8 @@
+/**
+ * @file 2-7-TypeConversion.c
+ * @author AaronLi
+ * @date 2022-12-07-下午3:28
+ * @description 
+ * @link 
+ */
+
--- cmake-build-debug/ChapterTwoTypeOperatorsAndExpressions/book/cmake_install.cmake
+++ cmake-build-debug/ChapterTwoTypeOperatorsAndExpressions/book/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/AaronLi/C_Workspace/ChapterTwoTypeOperatorsAndExpressions
+# Install script for directory: /home/AaronLi/C_Workspace/ChapterTwoTypeOperatorsAndExpressions/book
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,15 +40,5 @@ endif()
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/AaronLi/C_Workspace/cmake-build-debug/ChapterTwoTypeOperatorsAndExpressions/exercise/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/AaronLi/C_Workspace/cmake-build-debug/ChapterTwoTypeOperatorsAndExpressions/book/cmake_install.cmake")
 endif()
 
