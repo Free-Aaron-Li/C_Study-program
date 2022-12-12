70cd59771aa95c26412eba86165342a53da4bfc2
AaronLi <free_aaron.li@outlook.com>
2022-12-12 19:06:38 +0800

    更新

--- cmake-build-debug/GraphicsGame/Game1/cmake_install.cmake
+++ cmake-build-debug/GraphicsGame/Game1/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/AaronLi/C_Workspace/ChapterTwoTypeOperatorsAndExpressions
+# Install script for directory: /home/AaronLi/C_Workspace/GraphicsGame/Game1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,10 +40,5 @@ endif()
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/AaronLi/C_Workspace/cmake-build-debug/ChapterTwoTypeOperatorsAndExpressions/exercise/cmake_install.cmake")
 endif()
 
--- cmake-build-debug/GraphicsGame/cmake_install.cmake
+++ cmake-build-debug/GraphicsGame/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/AaronLi/C_Workspace/ChapterTwoTypeOperatorsAndExpressions
+# Install script for directory: /home/AaronLi/C_Workspace/GraphicsGame
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -44,6 +44,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/AaronLi/C_Workspace/cmake-build-debug/ChapterTwoTypeOperatorsAndExpressions/exercise/cmake_install.cmake")
+  include("/home/AaronLi/C_Workspace/cmake-build-debug/GraphicsGame/Game1/cmake_install.cmake")
 endif()
 
