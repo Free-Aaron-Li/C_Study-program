533304c5d3d70ea7daee6a1f30d72ff4731589a9
AaronLi <free_aaron.li@outlook.com>
2022-12-13 16:09:14 +0800

    更新

---
+++ src/ChapterTwoTypeOperatorsAndExpressions/src/exercise/2-4-Squeeze.c
@@ -0,0 +1,20 @@
+/**
+ * @file 2-4-Squeeze.c
+ * @author AaronLi
+ * @date 2022-12-13-上午11:07
+ * @description  重新编写squeeze(s1,s2)，将字符串s1中任何与字符串s2中的字符匹配的字符都删除
+ * @link 
+ */
+
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    int j = 1;
+    int num;
+    int num1;
+    num=i++;
+    printf("%d\n",num);
+    num1=++j;
+    printf("%d\n",num1);
+}
