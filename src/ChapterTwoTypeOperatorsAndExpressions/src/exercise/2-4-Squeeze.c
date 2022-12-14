5016f74c7b3ae8e1731f2ac930f4b808d8eb4ca8
AaronLi <free_aaron.li@outlook.com>
2022-12-14 12:31:53 +0800

    更新

--- src/ChapterTwoTypeOperatorsAndExpressions/src/exercise/2-4-Squeeze.c
+++ src/ChapterTwoTypeOperatorsAndExpressions/src/exercise/2-4-Squeeze.c
@@ -2,19 +2,61 @@
  * @file 2-4-Squeeze.c
  * @author AaronLi
  * @date 2022-12-13-上午11:07
- * @description  重新编写squeeze(s1,s2)，将字符串s1中任何与字符串s2中的字符匹配的字符都删除
+ * @description
  * @link 
  */
 
 #include <stdio.h>
 
+/**
+ * @date 22-12-13 下午5:02
+ * @description 自定义strcat函数，用来连接两个字符串
+ */
+
+void my_strcat(char s[], const char t[]) {
+    int i, j;
+    i = j = 0;
+
+    while (s[i] != '\0')        /* find end of s */
+        i++;
+    while ((s[i++] = t[j++]) != '\0');
+}
+
+/**
+ * @date 22-12-13 下午7:48
+ * @description squeeze函数删除指定字符
+ */
+
+void squeeze(char s[], char t) {
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; i++) {
+        if (s[i] != t)
+            s[j++] = s[i];
+    }
+    s[j] = '\0';
+}
+
+/**
+ * @date 22-12-13 下午9:00
+ * @description 编写delete函数，将字符串s1中任何和字符串s2中的字符匹配的字符都删除
+ */
+void delete(char s[], char t[]) {
+
+}
+
 int main() {
-    int i = 1;
-    int j = 1;
-    int num;
-    int num1;
-    num=i++;
-    printf("%d\n",num);
-    num1=++j;
-    printf("%d\n",num1);
+    char s[100] = "123 abc this is good day!";      /* Prevents arrays from crossing boundaries */
+    char e[100] = "45678998087098";
+    my_strcat(s, e);
+
+    for (int i = 0; i < 100 && s[i] != '\0'; ++i) {
+        printf("%c", s[i]);
+    }
+
+    printf("\n");
+    squeeze(s, ' ');
+    for (int i = 0; i < 100 && s[i] != '\0'; ++i) {
+        printf("%c", s[i]);
+    }
+
 }
